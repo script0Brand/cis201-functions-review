f93ef04a10891f4812963a8e87bccfa20aed7d3b
byron perez <byronperez@cis.vvc.edu>
2018-10-16 18:22:08 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,48 @@
 #include<iostream>
-
+#include<string>
 
 using namespace std;
+
+string s = "Apple valley CA is an American town";
+int count = 0;
+char c = 'a';
+char C = 'A';
+
+int total(string s);
 
 
 int main()
 {
-    cout << "Hello World"<< endl;
+    cout << total(s) << endl;
 
     return 0;
 }
+
+int total(string s)
+{
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s.at(i) == c || s.at(i) == C)
+            count++;            
+    }
+    return count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
